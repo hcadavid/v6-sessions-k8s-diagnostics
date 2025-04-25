63ddc1dd04e9cac2455bd3907d07a24c27b4d4d7
hcadavid <hector.cadavid@gmail.com>
2025-04-25 09:10:48 +0000

    partial method exeriment

--- v6-session-basics/partial.py
+++ v6-session-basics/partial.py
@@ -9,10 +9,13 @@ or directly to the user (if they requested partial results).
 
 import pandas as pd
 from vantage6.algorithm.decorator import data, source_database
+from vantage6.algorithm.client import AlgorithmClient
+from vantage6.algorithm.tools.decorators import algorithm_client, data
 from vantage6.algorithm.decorator.action import (
     data_extraction,
     pre_processing,
     federated,
+    central
 )
 
 
@@ -79,3 +82,67 @@ def federated_avg(df1: pd.DataFrame, column) -> dict:
         "count": int(local_count)
     }  
 
+
+@central
+@algorithm_client
+def central_average(client: AlgorithmClient, column_name: str):
+
+    # Info messages can help you when an algorithm crashes. These info
+    # messages are stored in a log file which is send to the server when
+    # either a task finished or crashes.
+    print("Collecting participating organizations")
+    print(f"Using client:{client}")
+
+    # task = client.task.create(name="fedavg", description="subtask", collaboration=1,
+    #                 organizations=[2,3,4],
+    #                 databases=[{"label": "s2_dframe1", "type": "dataframe", "dataframe_id":"2"}], 
+    #                 image="ghcr.io/hcadavid/v6-sessions-k8s-diagnostics:latest", method="federated_avg", 
+    #                 action="federated compute",
+    #                 input_={
+    #                     "args": ["Age"],
+    #                     "kwargs": {}
+    #                 }
+    # , session=2)
+
+    task = client.task.create(name="central-fedavg", description="subtask",
+                    organizations=[2,3,4],
+                    method="federated_avg",
+                    input_={
+                        "args": ["Age"],
+                        "kwargs": {}
+                    })
+
+    client.wait_for_results(task['job_id'])
+
+    # Collect all organization that participate in this collaboration.
+    # These organizations will receive the task to compute the partial.
+    #organizations = client.organization.list()
+    #ids = [organization.get("id") for organization in organizations]
+
+    # Request all participating parties to compute their partial. This
+    # will create a new task at the central server for them to pick up.
+    # We've used a kwarg but is is also possible to use `args`. Although
+    # we prefer kwargs as it is clearer.
+    #info("Requesting partial computation")
+    #task = client.task.create(
+    #    input_={"method": "partial_average", "kwargs": {"column_name": column_name}},
+    #    organizations=ids,
+    #)
+
+    # Now we need to wait until all organizations(/nodes) finished
+    # their partial. We do this by polling the server for results. It is
+    # also possible to subscribe to a websocket channel to get status
+    # updates.
+    # info("Waiting for results")
+    # results = client.wait_for_results(task_id=task.get("id"))
+    # info("Partial results are in!")
+
+    # # Now we can combine the partials to a global average.
+    # info("Computing global average")
+    # global_sum = 0
+    # global_count = 0
+    # for output in results:
+    #     global_sum += output["sum"]
+    #     global_count += output["count"]
+
+    return {"average": 0.5}
