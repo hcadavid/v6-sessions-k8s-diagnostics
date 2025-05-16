e5c5aa1f914aa63ea247b52f7627dd62b947c0a7
hcadavid <hector.cadavid@gmail.com>
2025-05-16 14:22:51 +0000

    Central average function working with sessions

--- v6-session-basics/partial.py
+++ v6-session-basics/partial.py
@@ -246,35 +246,26 @@ def sleep(client: AlgorithmClient, sleep_time:int):
 
 @central
 @algorithm_client
-def central_average(client: AlgorithmClient, column_name: str):
+def central_average(client: AlgorithmClient, column_name: str, session_id: int):
 
     # Info messages can help you when an algorithm crashes. These info
     # messages are stored in a log file which is send to the server when
     # either a task finished or crashes.
     print("Collecting participating organizations")
     
-
-    # task = client.task.create(name="fedavg", description="subtask", collaboration=1,
-    #                 organizations=[2,3,4],
-    #                 databases=[{"label": "s2_dframe1", "type": "dataframe", "dataframe_id":"2"}], 
-    #                 image="ghcr.io/hcadavid/v6-sessions-k8s-diagnostics:latest", method="federated_avg", 
-    #                 action="federated compute",
-    #                 input_={
-    #                     "args": ["Age"],
-    #                     "kwargs": {}
-    #                 }
-    # , session=2)
+    # Collect all organization that participate in this collaboration.
+    # These organizations will receive the task to compute the partial.
+    organizations = client.organization.list()
+    ids = [organization.get("id") for organization in organizations]
 
     task = client.task.create(name="central-fedavg", description="subtask",
-                    organizations=[2,3,4],                    
+                    organizations=ids,                    
                     input_={
                         "method": "federated_avg",
                         "args": ["Age"],
                         "kwargs": {}
-                    }, session=1)
+                    }, session=session_id)
 
-    info("Waiting 10 seconds")
-    time.sleep(10)
     info("Waiting for results...")
     results = client.wait_for_results(task_id=task.get("id"))
     info("Partial results are in!")
@@ -288,47 +279,5 @@ def central_average(client: AlgorithmClient, column_name: str):
 
     print(f">>>{global_sum}")
     print(f">>>{global_count}")
-    # Now we can combine the partials to a global average.
-    #info("Computing global average")
-    #global_sum = 0
-    #global_count = 0
-    #for output in results:
-    #    global_sum += output["sum"]
-    #    global_count += output["count"]
 
-    return {"average": 0.5}
-
-    #client.wait_for_results(task['job_id'])
-
-    # Collect all organization that participate in this collaboration.
-    # These organizations will receive the task to compute the partial.
-    #organizations = client.organization.list()
-    #ids = [organization.get("id") for organization in organizations]
-
-    # Request all participating parties to compute their partial. This
-    # will create a new task at the central server for them to pick up.
-    # We've used a kwarg but is is also possible to use `args`. Although
-    # we prefer kwargs as it is clearer.
-    #info("Requesting partial computation")
-    #task = client.task.create(
-    #    input_={"method": "partial_average", "kwargs": {"column_name": column_name}},
-    #    organizations=ids,
-    #)
-
-    # Now we need to wait until all organizations(/nodes) finished
-    # their partial. We do this by polling the server for results. It is
-    # also possible to subscribe to a websocket channel to get status
-    # updates.
-    # info("Waiting for results")
-    # results = client.wait_for_results(task_id=task.get("id"))
-    # info("Partial results are in!")
-
-    # # Now we can combine the partials to a global average.
-    # info("Computing global average")
-    # global_sum = 0
-    # global_count = 0
-    # for output in results:
-    #     global_sum += output["sum"]
-    #     global_count += output["count"]
-
-    #return {"average": 0.5}
+    return {"average": global_sum / global_count}
