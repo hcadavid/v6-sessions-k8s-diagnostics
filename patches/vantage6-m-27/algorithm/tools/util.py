1eae814c843a5a61c3760d1d57cad5ba3493b4e9
hcadavid <hector.cadavid@gmail.com>
2025-05-27 16:04:01 +0000

    patching with updated alg-tools

--- patches/vantage6-m-27/algorithm/tools/util.py
+++ patches/vantage6-m-27/algorithm/tools/util.py
@@ -202,10 +202,15 @@ def get_action() -> AlgorithmStepType:
 
     requested_action = os.environ[ContainerEnvNames.FUNCTION_ACTION.value]
     try:
+        
+        print(">>>>>>AlgorithmStepType enum:")
+        for attr in dir(AlgorithmStepType):
+            if not attr.startswith("__"):
+                print(f"  {attr}: {getattr(AlgorithmStepType, attr)}")
         action = AlgorithmStepType(requested_action)
     except ValueError as exc:
         raise EnvironmentVariableError(
-            f"Environment variable {ContainerEnvNames.FUNCTION_ACTION.value} has value "
+            f">>>>>>Environment variable {ContainerEnvNames.FUNCTION_ACTION.value} has value "
             f"'{requested_action}' which is not a valid action."
         ) from exc
 
